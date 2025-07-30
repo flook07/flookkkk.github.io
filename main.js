a3c5270a8c6ce459ba800743dda2f3a8d2b4596c
flook07 <wiswa.2007@gmail.com>
2025-07-30 16:56:43 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,23 @@
+function wish(){
+        document.getElementById("wish").style.display = "block";
+    }
+function album1(){
+        document.getElementById('album1').style.display = "block"}
+    function album2(){
+        document.getElementById('album2').style.display = "block"}
+    function album3(){
+        document.getElementById('album3').style.display = "block"}
+    function album4(){
+        document.getElementById('album4').style.display = "block"}
+    function album5(){
+        document.getElementById('album5').style.display = "block"}
+    function album6(){
+        document.getElementById('album6').style.display = "block"}
+    function album7(){
+        document.getElementById('album7').style.display = "block"}
+    function album8(){
+        document.getElementById('album8').style.display = "block"}
+    function album9(){
+        document.getElementById('album9').style.display = "block"}
+    function album10(){
+        document.getElementById('album10').style.display = "block"}
